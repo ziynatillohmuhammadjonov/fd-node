b9893c456c743966cd7face1c1dd624b56c7acd7
Ziynatillo <zmorzugrand@gmail.com>
2024-02-22 17:46:18 +0500

    Import bo'yicha topshiriq

--- index.ts
+++ index.ts
@@ -51,41 +51,93 @@
 
 import mongoose from 'mongoose'
 
-mongoose.connect('mongodb://localhost/test').then(() => {
-    console.log('MongoDBga ulanish hosil qildim...');
-}).catch((err) => {
-    console.log("MongoDbga ulanishda xatolk bo'ldi " + err);
+// mongoose.connect('mongodb://localhost/test').then(() => {
+//     console.log('MongoDBga ulanish hosil qildim...');
+// }).catch((err) => {
+//     console.log("MongoDbga ulanishda xatolk bo'ldi " + err);
+// })
+
+// const bookSchema = new mongoose.Schema({
+//     name: String,
+//     author: String,
+//     tags: [String],
+//     date: { type: Date, default: Date.now },//schemani default qiymatini qo'yish
+//     isPublished: Boolean
+// })
+// const Book = mongoose.model("Book", bookSchema)
+
+
+// async function createBook() {
+//     const book = new Book({
+//         name: "NodeJs to'liq qo'llanma",
+//         author: 'Farhod Dadajonov',
+//         tags: ['js', 'dasturlash', 'node'],
+//         isPublished: false
+//     })
+//     const saveBook = await book.save()
+//     console.log(saveBook);
+// }
+// // createBook()
+
+// async function getBook() {
+//     const books = await Book.find({
+//         author: 'Farhod Dadajonov'
+//     }).limit(1).sort({ name: 1 }).select({ name: 1, tags: 1 });
+//     console.log(books);
+
+// }
+// // getBook()
+
+// // Paginatsiyani amalga oshirish uchun uni .limit va .skip metodlari orqali amalga 
+// async function getBookWithPagination() {
+//     const pageNumber = 3;
+//     const pageSize = 10
+//     // /api/books?pageNumber=3&pageSize=10
+//     const books = await Book.find({
+//         author: 'Farhod Dadajonov'
+//     })
+//     .skip((pageNumber-1)*pageSize)
+//     .limit(pageSize)
+//     .sort({ name: 1 })
+//     .select({ name: 1, tags: 1 });
+//     console.log(books);
+
+// }
+
+// // getBookWithPagination()
+
+mongoose.connect('mongodb://localhost/usersTest').then(()=>{
+    console.log("MondoDb ga ulanish hosil qilmdim..");
+}).catch((err)=>{
+    console.log('MongoDbga ulanishda xatolik sodir boldi '+err);
 })
 
-const bookSchema = new mongoose.Schema({
+
+interface iUser {
     name: String,
-    author: String,
-    tags: [String],
-    date: { type: Date, default: Date.now },//schemani default qiymatini qo'yish
-    isPublished: Boolean
+    phoneNumber:String,
+    email: String,
+    address:String,
+    userAgent: String,
+    hexcolor:String
+}
+
+const user = new  mongoose.Schema<iUser>({
+    name: {type: String, required:true},
+    phoneNumber:{type: String, required:true},
+    email: {type: String, required:true},
+    address:{type: String, required:true},
+    userAgent: {type: String, required:true},
+    hexcolor:{type: String, required:true}
 })
-const Book = mongoose.model("Book", bookSchema)
 
+const User = mongoose.model('User', user)
 
-async function createBook() {
-    const book = new Book({
-        name: "NodeJs to'liq qo'llanma",
-        author: 'Farhod Dadajonov',
-        tags: ['js', 'dasturlash', 'node'],
-        isPublished: false
-    })
-    const saveBook = await book.save()
-    console.log(saveBook);
-
+async function getUser() {
+    const users =await User
+    .find()
+    .select({name:1, hexcolor:1})
+    console.log(users);
+    
 }
-// createBook()
-
-
-async function getBook() {
-    const books = await Book.find({
-        author: 'Farhod Dadajonov'
-    }).limit(1).sort({ name: 1 }).select({ name: 1, tags: 1 });
-    console.log(books);
-
-}
-getBook()
+getUser()
